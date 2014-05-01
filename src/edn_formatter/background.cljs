68f9f667caa60f9ab59538a6cf2ec14a85457056
Conner Petzold <conner@getprismatic.com>
2014-04-30 23:08:30 -0700

    Only run content-script body if the request is application/edn

--- src/edn_formatter/background.cljs
+++ src/edn_formatter/background.cljs
@@ -4,6 +4,8 @@
 
 (def +edn-mime-type-regexp+ #"application/edn")
 
+(def edn-urls (atom #{}))
+
 (defn is-header? [header k]
   (= (.toLowerCase (name k))
      (.toLowerCase (:name header))))
@@ -27,14 +29,20 @@
 (when background?
   (on-headers-recieved
    (fn [details]
+
      (-> details
          (select-keys [:responseHeaders])
          (update-in
           [:responseHeaders] update-header :content-type
           (fn [content-type]
             (if (re-find +edn-mime-type-regexp+ content-type)
-              "text/edn"
+              (do (swap! edn-urls conj (:url details))
+                  "text/edn")
               content-type)))))
    {:urls ["<all_urls>"]
     :types ["main_frame" "sub_frame"]}
-   ["blocking" "responseHeaders"]))
+   ["blocking" "responseHeaders"])
+
+  (.addListener js/chrome.runtime.onMessage
+                (fn [req sender respond]
+                  (respond (contains? @edn-urls (.-url sender))))))
--- src/edn_formatter/content.cljs
+++ src/edn_formatter/content.cljs
@@ -3,25 +3,36 @@
    [cljs.reader :as reader]
    [om.core :as om :include-macros true]
    [ankha.core :as ankha]
-   [edn-formatter.util :refer [background?]]))
+   [edn-formatter.util :refer [background?]]
+   [edn-formatter.css :as css]))
 
 (defn div [id]
   (let [el (js/document.createElement "div")]
     (set! (.-id el) id)
     el))
 
+(defn send-message [message cb]
+  (.sendMessage js/chrome.runtime (clj->js message)
+                #(cb (js->clj % :keywordize-keys true))))
+
+(defn edn-request? [cb]
+  (send-message "edn?" cb))
+
 (defn init-format [e]
-  (let [pre js/document.body.firstChild
-        edn-str (.-textContent pre)
-        edn (reader/read-string edn-str)
-        container (div "container")]
-    (.removeChild js/document.body pre)
-    (.appendChild js/document.body container)
+  (edn-request?
+   (fn [edn?]
+     (when edn?
+       (let [pre js/document.body.firstChild
+             edn-str (.-textContent pre)
+             edn (reader/read-string edn-str)
+             container (div "container")]
+         (.removeChild js/document.body pre)
+         (.appendChild js/document.body container)
 
-    (js/console.log "%cEDN Formatter" "font-weight: bold")
-    (js/console.log (clj->js edn))
+         (js/console.log "%cEDN Formatter" "font-weight: bold")
+         (js/console.log (clj->js edn))
 
-    (om/root ankha/inspector edn {:target container})))
+         (om/root ankha/inspector edn {:target container}))))))
 
 (when-not background?
   (.addEventListener
